6d46ac66cdba62c9f5d8a806267fa543235ac88b
Prerita.CG <prerita.codingal@gmail.com>
2025-05-22 09:08:06 +0530

    trial class update

--- AIEPCM2L1A_Intro_to_Computer_Vision___OpenCV-a66d/a1.py
+++ AIEPCM2L1A_Intro_to_Computer_Vision___OpenCV-a66d/a1.py
@@ -1,10 +1,21 @@
 import cv2
+import matplotlib.pyplot as plt
+
+print(cv2.getBuildInformation())
 
 # Load the image
-image = cv2.imread('example.jpg')
+image = cv2.imread("example.jpg")
+if image is None:
+    print("Error: Image not loaded.")
+else:
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  # Convert BGR to RGB
+    plt.axis("off")  # Hide axes
+    plt.show()  # Display the image
+
 
 # Resize the window to a specific size without resizing the image
-cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)  # Create a resizable window
+#cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)  # Create a resizable window
+cv2.imshow('Loaded Image', image)
 cv2.resizeWindow('Loaded Image', 800, 500)  # Set the window size to 800x500 (width x height)
 
 # Display the image in the resized window
