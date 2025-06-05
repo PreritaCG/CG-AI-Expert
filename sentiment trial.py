57ef62836da502430592cfeed0b83f1182eed5ed
Prerita.CG <prerita.codingal@gmail.com>
2025-06-06 02:19:27 +0530

    updates

--- sentiment trial.py
+++ sentiment trial.py
@@ -2,7 +2,7 @@ from textblob import TextBlob
 sentence = input("Enter a sentence")
 blob = TextBlob(sentence)
 sentiment = blob.sentiment.polarity
-if sentiment > 8:
+if sentiment>0:
  print("positive")
 elif sentiment < 0:
  print("negative")
